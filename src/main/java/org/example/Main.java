e3ad212fda8096e044a2077ccdd9bc936019b5b4
seungwoo1209 <2002msw@naver.com>
2025-02-25 13:54:51 +0900

    1167: 트리의 지름

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,46 +7,84 @@ import java.util.*;
 
 class Main {
 
-    static Long A;
-    static Long B;
-    static Long C;
-    static Long reminder;
+    static ArrayList<node>[] arr;
+    static Long max = 0L;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        A = Long.parseLong(st.nextToken());
-        B = Long.parseLong(st.nextToken());
-        C = Long.parseLong(st.nextToken());
+        visited = new boolean[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            visited[i] = false;
+        }
 
-        reminder = A % C;
+        arr = new ArrayList[n+1];
+        for (int i = 0; i < n + 1; i++) {
+            arr[i] = new ArrayList<node>();
+        }
 
+        for(int i=0;i<n;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int parent = Integer.parseInt(st.nextToken());
+            int child;
+            while((child = Integer.parseInt(st.nextToken()))!=-1){
+                int weight = Integer.parseInt(st.nextToken());
+                arr[parent].add(new node(child,weight));
+            }
+        }
 
-        System.out.println(calc(B));
+        Long calc = dfs(1);
+        if(max < calc){
+            max = calc;
+        }
+
+        System.out.println(max);
 
     }
 
-    static Long calc(Long B) {
-        if (B == 1) {
-            return reminder;
-        } else {
-            Long temp = calc(B/2);
-
-            if (B % 2 != 0)
-                return (temp * temp) % C * reminder % C;
-            else
-                return (temp * temp) % C;
+    private static Long dfs(int i) {
+        visited[i] = true;
+        ArrayList<Long> results = new ArrayList<>();
+        for(int j=0;j<arr[i].size();j++){
+            node n = arr[i].get(j);
+            if(!visited[n.child]){
+                results.add(dfs(n.child) + n.weight);
+            }
         }
+
+        if(results.isEmpty()){
+            // 자식 없으면 0
+           return 0L;
+        } else if(results.size() == 1){
+            // 자식 하나면 그거
+            return results.get(0);
+        } else {
+            // 여기서 자식 두개로 경로를 형성하는 것이 최댓값이라면
+            Collections.sort(results, Collections.reverseOrder());
+            Long currMax = results.get(0) + results.get(1);
+            if(max < currMax){
+                max = currMax;
+            }
+
+            // 자식 중에 제일 큰거
+            return results.get(0);
+        }
+
+
+    }
+
+}
+
+class node {
+    public int child;
+    public int weight;
+
+    public node(int child, int weight) {
+        this.child = child;
+        this.weight = weight;
     }
 }
 
-//2147483646 2147483646 2147483647
 
-//        for(int i=0;i<3;i++){
-//            for(int j=0;j<N;j++){
-//                System.out.print(cost[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
