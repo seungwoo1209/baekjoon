7d698b08d60f9248ab7cde86dcffa4ba3c87b94f
seungwoo1209 <2002msw@naver.com>
2025-03-01 22:03:30 +0900

    1238 solving

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,49 +3,110 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.MAX_VALUE;
 
 class Main {
 
-    static int N;
-    static Long sum = 0L;
-    static boolean[] visitedRow;
-    static boolean[] visitedSum;
-    static boolean[] visitedDiff;
-    static int currentQueenCount;
+    static int N, M, X;
+    static ArrayList<Node>[] graph;
+    static int[][] dp;
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        X = Integer.parseInt(st.nextToken());
 
-        visitedRow = new boolean[N];
-        visitedSum = new boolean[2 * N - 1];
-        visitedDiff = new boolean[2 * N - 1];
+        graph = new ArrayList[N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            graph[i] = new ArrayList<Node>();
+        }
 
-        calc(0);
-        System.out.println(sum);
+        dp = new int[N +1][N +1];
+
+        for(int i = 0; i< M; i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            graph[start].add(new Node(end, weight));
+        }
+
+        floyd();
+
+        int max = Integer.MIN_VALUE;
+        for(int i=1;i<=N;i++){
+            if(i == X) continue;
+            max = Math.max((dp[X][i] + dp[i][X]), max);
+        }
+        System.out.println(max);
     }
 
-    static void calc(int colindex) {
-        if (currentQueenCount == N) {
-            sum++;
-            return;
+    private static void floyd() {
+        // dp[?][i][j] : ?번 노드 포함시켰을때. 0번은 그냥 원래꺼 복사.
+        for(int i = 0; i< N +1; i++){
+            for(int j = 0; j< N +1; j++){
+                dp[i][j] = MAX_VALUE;
+            }
         }
 
-        for (int i = 0; i < N; i++) {
-            // 못 가는 칸인 경우
-            if (visitedRow[i] || visitedSum[i + colindex] || visitedDiff[N - 1 + i - colindex]) continue;
-            // 갈수 있는 칸인 경우, 현재 좌표는 row: i, col: colindex
-            visitedRow[i] = true;
-            visitedSum[i + colindex] = true;
-            visitedDiff[N - 1 + i - colindex] = true;
-            currentQueenCount++;
-            // 해당 칸에 이제 놓은 것. 다음 행으로
-            calc(colindex + 1);
-            // 놓은 것 취소
-            visitedRow[i] = false;
-            visitedSum[i + colindex] = false;
-            visitedDiff[N - 1 + i - colindex] = false;
-            currentQueenCount--;
+        for (int i = 1; i < N + 1; i++) {
+            for(int j=0;j<graph[i].size();j++){
+                Node n = graph[i].get(j);
+                dp[i][n.to] = n.weight;
+            }
         }
+
+        // 1번 노드부터 n번 노드까지 포함시키기
+        for(int k = 1; k<= N; k++){
+            // 플로이드에서의 모든 노드의 모든 노드간의 가중치 업데이트를 해줄 필요가 없고,
+            // 현재 노드 -> (현재까지 고려된 각 노드)까지의 최단 거리 + 파티까지의 최종 거리
+
+            //추가한 노드까지의 최단거리 업데이트
+
+            // 각 노드의 X까지의 거리 업데이트
+            for(int i = 1; i<= N; i++){
+                if(i == X) continue;
+                if(dp[i][k] != MAX_VALUE){
+                    for(int j=0;j<graph[k].size();j++){
+                        Node n = graph[k].get(j);
+                        if(n.to == i) continue;
+                        dp[i][n.to] = Math.min(dp[i][n.to], dp[i][k] + n.weight);
+                    }
+                }
+
+                int weightByPassingNodeK = (dp[i][k] == MAX_VALUE) || (dp[k][X] == MAX_VALUE) ?
+                        MAX_VALUE :
+                        dp[i][k] + dp[k][X];
+                dp[i][X] = Math.min(dp[i][X], weightByPassingNodeK);
+            }
+
+            if(dp[X][k] != MAX_VALUE){
+                for(int j=0;j<graph[k].size();j++){
+                    Node n = graph[k].get(j);
+                    if(n.to == X) continue;
+                    dp[X][n.to] = Math.min(dp[X][n.to], dp[X][k] + n.weight);
+                }
+            }
+
+        }
+
+    }
+}
+
+
+class Node {
+    public int to;
+    public int weight;
+
+    public Node(int to, int weight) {
+        this.to = to;
+        this.weight = weight;
     }
 }
